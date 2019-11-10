da741440f1a4886e814b28e57713b22473c63c25
Akshat Adsule <akshat.adsule@gmail.com>
2019-11-10 10:16:44 -0800

    Added datetime features

---
+++ defs.py
@@ -0,0 +1,25 @@
+def getMonth(x):
+    if (x == 1):
+        return "January"
+    if (x == 2):
+        return "Febuary"
+    if (x == 3):
+        return "March"
+    if (x == 4):
+        return "April"
+    if (x == 5):
+        return "May"
+    if (x == 6):
+        return "June"
+    if ( x== 7):
+        return "July"
+    if (x == 8):
+        return "August"
+    if (x == 9):
+        return "September"
+    if (x == 10):
+        return "October"
+    if (x == 11):
+        return "November"
+    if (x == 12):
+        return "December"
--- tweetText.py
+++ tweetText.py
@@ -1,11 +1,22 @@
 
 from twython import Twython
+import datetime as time
+from defs import getMonth
 from auth import (
     consumer_key,
     consumer_secret,
     access_token,
     access_token_secret
 )
+
+now = time.datetime.now()
+
+yearint = now.year
+month_str = getMonth(now.month)
+dayint = now.day
+
+year_str = str(yearint)
+day_str = str(dayint)
 
 twitter = Twython(
     consumer_key,
@@ -14,6 +25,7 @@ twitter = Twython(
     access_token_secret
 )
 
-message = "Hello World!"
-twitter.update_status(status=message)
-print("Tweeted: " + message)
+message = input("What do you want to Tweet? ")
+finalMessage = "This was Tweeted automaticaly on " + month_str + " " + day_str + ", " + year_str + ": " + message
+print(finalMessage)
+twitter.update_status(status=finalMessage)
