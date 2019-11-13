757a40c683c4021c14c454e86f967b7ad5725945
Akshat Adsule <akshat.adsule@gmail.com>
2019-11-12 18:45:29 -0800

    Changed main files

--- examples/update_status.py
+++ examples/update_status.py
@@ -6,4 +6,4 @@ twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 try:
     twitter.update_status(status='See how easy this was?')
 except TwythonError as e:
-    print (e)
+    print e
