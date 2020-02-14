ef25bfd586d48fc1357310efd2d7102270418ec9
Akshat Adsule <akshat.adsule@gmail.com>
2020-02-14 13:59:35 -0800

    custom city feature

--- tweetISS.py
+++ tweetISS.py
@@ -1,9 +1,28 @@
 import requests
 from datetime import datetime
+from opencage.geocoder import OpenCageGeocode
+
+key = 'a00614947d4d49eebe9a0e5e919f973a'
+
+geocoder = OpenCageGeocode(key)
+
+input = input("Enter a city or adress to find when the ISS will go over you: ")
+query = input
+results = geocoder.geocode(query)
+
+output = (u'%f;%f;%s;%s' % (results[0]['geometry']['lat'], 
+                        results[0]['geometry']['lng'],
+                        results[0]['components']['country_code'],
+                        results[0]['annotations']['timezone']['name']))
+
+print (output)
+outputstr = str(output)
+outputstr = output.split(";")
+cityout = outputstr[3]
 
 parameters = {
-    "lat": 37.3229978,
-    "lon": -122.0321823 }
+    "lat": outputstr[0],
+    "lon": outputstr[1] }
 
 response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
 pass_times = response.json()['response']
@@ -18,5 +37,5 @@ times = []
 for rt in risetimes:
     time = datetime.fromtimestamp(rt)
     times.append(time)
-    print("The ISS will be over you on ", time)
+    print("The ISS will be over ", input, " on ", time)
     #print(time)
